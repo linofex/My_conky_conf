f9c9420080bc78b878d1d76d9478a355eb1d90a2
linofex <alessandro.noferi@gmail.com>
2018-11-05 12:26:28 +0100

    new versions

--- conky_versions/conkyrc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sleep 5
-#conky -q -c /home/linofex/.conkyrc &
-conky -q -c /home/linofex/conkyrc2
- exit
--- conky_versions/conkyrc_mezzo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sleep 5
-#conky -q -c /home/linofex/.conkyrc &
-conky -q -c /home/linofex/.conky/conky_mezzo_sx
-conky -q -c /home/linofex/.conky/conky_mezzo_dx
-
-
- exit
